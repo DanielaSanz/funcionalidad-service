0cf5510ad10e99727becb0ff7c0fd0744ee34c1b
unknown <sanchezdanielacv@gmail.com>
2020-06-19 19:24:12 -0300

    initial commit

---
+++ src/main/java/com/service/funtionality/FuntionalityApplication.java
@@ -0,0 +1,13 @@
+package com.service.funtionality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuntionalityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FuntionalityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/service/funtionality/FuntionalityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.funtionality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuntionalityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
